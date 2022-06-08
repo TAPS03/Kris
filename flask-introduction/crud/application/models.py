ffa2ec290e862d5789744baf6f5d19983d633aa6
Kristaps <kliepins6@gmail.com>
2022-06-08 16:33:52 +0000

    changes

--- flask-introduction/crud/application/models.py
+++ flask-introduction/crud/application/models.py
@@ -3,6 +3,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, EqualTo
 
+
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     f_name = db.Column(db.String(30), nullable= False)
@@ -10,9 +11,17 @@ class Customer(db.Model):
     email = db.Column(db.String(40), nullable= False)
     username = db.Column(db.String(40), nullable= False)
     password = db.Column(db.String(40), nullable= False)
-    confirm_password= db.Column(db.String(40), nullable= False)
     address = db.Column(db.String(30), nullable= False)
     postcode = db.Column(db.String(10), nullable= False)
+#     orders = db.relationship('Orders', backref='customer', lazy= True)
+
+# class Orders(db.Model):
+#     customer_id= db.Column(db.Integer, db.ForeignKey('customer_id', nullable= False))
+#     order_number = db.Column(db.String(15), nullable= False)
+#     deliver_address = db.Column(db.String(50), nullable= False)
+#     amount = db.Column(db.Float(40), nullable= False)
+   
+
 
 
 
@@ -22,12 +31,17 @@ class CustomerForm(FlaskForm):
     f_name = StringField("First Name:" , validators=[DataRequired()] )
     l_name = StringField("Last Name: ", validators=[DataRequired()] )
     email = StringField("Email Address:", validators=[DataRequired()] )
-    username = StringField("Username:", validators=[DataRequired()] )
-    password = PasswordField("Password:", validators=[DataRequired(), EqualTo('confirm_password')] )
+    username = StringField("Username:", validators=[DataRequired()], )
+    password = PasswordField("Password:", validators=[DataRequired()] )
     confirm_password = PasswordField("Confirm Password:", validators=[DataRequired()] )
     address = StringField("Street number and Name:", validators=[DataRequired()] )
     postcode = StringField("PostCode:", validators=[DataRequired()] )
     submit = SubmitField("Create")
+
+
+
+
+
 
 
 
--- flask-introduction/crud/application/routes.py
+++ flask-introduction/crud/application/routes.py
@@ -1,7 +1,7 @@
 from application import app, db
 from flask import render_template, request
 from application.models import CustomerForm, Customer
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 @app.route('/')
@@ -28,7 +28,11 @@ def add_customer():
     form= CustomerForm()
     if request.method == 'POST':
         if form.validate_on_submit():
-            new_customer= Customer(f_name=form.f_name.data, l_name=form.l_name.data, email=form.email.data, username=form.username.data, password=form.password.data, confirm_password=form.confirm_password.data ,address=form.address.data ,postcode=form.postcode.data)
+            password_to_save=generate_password_hash(form.password.data("password"), method='sha256')
+            new_customer= Customer(f_name=form.f_name.data, l_name=form.l_name.data, email=form.email.data, username=form.username.data, password=password_to_save, confirm_password=password_to_save ,address=form.address.data ,postcode=form.postcode.data)
+            if form.get('password') !=form.get('confirm_password'): 
+                return render_template('account.html', message = "passwords do not match")
+            user = user.query.filter_by(username=user).first()
             db.session.add(new_customer)
             db.session.commit()
             return render_template('home.html', message = "Thank you {f_name} {l_name}, your account has been created.")
