4baaed33bccb7be28d40fd289cf2352183ec575d
Kristaps <kliepins6@gmail.com>
2022-06-15 16:45:36 +0000

    restart

--- flask-introduction/crud/application/routes.py
+++ flask-introduction/crud/application/routes.py
@@ -1,17 +1,8 @@
+from ast import Compare
 from application import app, db
-from flask import render_template, request
-from application.models import CustomerForm, Customer
-from werkzeug.security import generate_password_hash, check_password_hash
-
-
-@app.route('/')
-def home():
-    return render_template('home.html')
-
-@app.route('/placeOrder')
-def placeOrder():
-    return render_template('placeOrder.html')
-
+from flask import redirect, render_template, request
+from application.models import CustomerForm, Customer, Thoughts, ThoughtsForm
+import pdb
 
 
 @app.route('/contact')
@@ -20,23 +11,35 @@ def contact():
 
 @app.route('/home')
 def realhome():
-    return render_template('home.html')
+    return render_template('home.html') 
 
 
-@app.route('/account', methods=["GET", "POST"])
+@app.route('/', methods=["GET", "POST"])
 def add_customer():
     form= CustomerForm()
     if request.method == 'POST':
         if form.validate_on_submit():
-            password_to_save=generate_password_hash(form.password.data("password"), method='sha256')
-            new_customer= Customer(f_name=form.f_name.data, l_name=form.l_name.data, email=form.email.data, username=form.username.data, password=password_to_save, confirm_password=password_to_save ,address=form.address.data ,postcode=form.postcode.data)
-            if form.get('password') !=form.get('confirm_password'): 
-                return render_template('account.html', message = "passwords do not match")
-            user = user.query.filter_by(username=user).first()
+            new_customer= Customer(f_name=form.f_name.data, l_name=form.l_name.data)
             db.session.add(new_customer)
             db.session.commit()
-            return render_template('home.html', message = "Thank you {f_name} {l_name}, your account has been created.")
-    else:
-        return render_template('account.html', form=form)
+            return render_template('put_thought.html', message = "Thank you {f_name} {l_name}, you have been added.")
+    return render_template('add.html', form=form)
+
+
+@app.route('/put_thought', methods=["GET", "POST"])
+def put_thought():
+    put_thought_form= ThoughtsForm()
+    customers = Customer.query.all()
+    for Customer in customers:
+        put_thought_form.persons_thoughts.choices.append((Customer.id, f"(Customer,f_name)" f"(Customer.l_name"))
+    if request.method == 'POST':
+        if put_thought_form.validate_on_submit():
+            new_thought= Thoughts(favourite_book=put_thought_form.favourite_book.data,rating= put_thought_form.rating.data,why_like_book=put_thought_form.why_like_book.data,compare=put_thought_form.compare.data,customer_id= put_thought_form.persons_thoughts.data) 
+            db.session.add(new_thought)
+            db.session.commit()
+            return render_template('account.html')
+    return render_template('/put_thought', form= put_thought_form)
+   
+        
 
 
