39b4342de18a795f672dd255253e47473719c213
Kristaps <kliepins6@gmail.com>
2022-06-15 16:45:49 +0000

    retry

--- flask-introduction/crud/application/models.py
+++ flask-introduction/crud/application/models.py
@@ -1,48 +1,36 @@
 from application import db
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, EqualTo
+from wtforms import StringField, SubmitField, SelectField
+from wtforms.validators import DataRequired
 
 
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     f_name = db.Column(db.String(30), nullable= False)
     l_name = db.Column(db.String(30), nullable= False)
-    email = db.Column(db.String(40), nullable= False)
-    username = db.Column(db.String(40), nullable= False)
-    password = db.Column(db.String(40), nullable= False)
-    address = db.Column(db.String(30), nullable= False)
-    postcode = db.Column(db.String(10), nullable= False)
-#     orders = db.relationship('Orders', backref='customer', lazy= True)
-
-# class Orders(db.Model):
-#     customer_id= db.Column(db.Integer, db.ForeignKey('customer_id', nullable= False))
-#     order_number = db.Column(db.String(15), nullable= False)
-#     deliver_address = db.Column(db.String(50), nullable= False)
-#     amount = db.Column(db.Float(40), nullable= False)
-   
+    thoughts = db.relationship('Thoughts', backref='customer')
 
 
+class Thoughts(db.Model):
+    id= db.Column(db.Integer, primary_key=True)
+    favourite_book= db.Column(db.String(40), nullable= False)
+    rating= db.Column(db.String(10), nullable= False)
+    why_like_book= db.Column(db.String(50), nullable= False)
+    compare= db.Column(db.String(30), nullable= False)
+    persons_thoughts= db.Column(db.String(30), nullable= False)
+    customer_id= db.Column(db.Integer, db.ForeignKey('customer.id'))
 
-
-
+class ThoughtsForm(FlaskForm):
+    favourite_book=StringField("What is your favourite book: ", validators=[DataRequired()])
+    rating= SelectField("Out of 10, what do you rate this book: ", choices= [("1","1"),("2","2"),("3","3"),("4","4"),("5","5"),("6","6"),("7","7"),("8","8"),("9","9"),("10","10")],  validators=[DataRequired()])
+    why_like_book= StringField("Why is this your favourite book: ",  validators=[DataRequired()])
+    compare= SelectField("Compared to your favourite book... which one of these would you be most likely to read: ", choices= [("Harry Potter","Harry Potter"), ("The Paper Palace","The Paper Palace"), ("The Island Of Missing Trees","The Island Of Missing Trees"), ("Sunset","Sunset")], validators=[DataRequired()])
+    persons_thoughts = SelectField("Person's opinion: " ,choices=[])
+    submit = SubmitField("Add Thoughts")
 
 class CustomerForm(FlaskForm):
     f_name = StringField("First Name:" , validators=[DataRequired()] )
     l_name = StringField("Last Name: ", validators=[DataRequired()] )
-    email = StringField("Email Address:", validators=[DataRequired()] )
-    username = StringField("Username:", validators=[DataRequired()], )
-    password = PasswordField("Password:", validators=[DataRequired()] )
-    confirm_password = PasswordField("Confirm Password:", validators=[DataRequired()] )
-    address = StringField("Street number and Name:", validators=[DataRequired()] )
-    postcode = StringField("PostCode:", validators=[DataRequired()] )
     submit = SubmitField("Create")
 
 
-
-
-
-
-
-
-db.create_all()
